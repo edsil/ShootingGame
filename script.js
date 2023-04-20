1c10e62855578b3b70548b9b6a0882233bd29793
Edu <silva.eduardo@gmail.com>
2023-04-21 00:19:30 +0100

    mi

--- script.js
+++ script.js
@@ -5,16 +5,33 @@ var projectiles = [];
 var maxV = 30;
 // Player
 var playerSize = 10;
-var playerSpeed = 1.4;
-var playerPos = { x: 300, y: 300 };
+var playerSpeed = 2.4;
+var playerInitialPos = { x: 0.5, y: 0.465 };
+// Gravity
+var g = 0.0;
+var bounciness = 1;
 // Walls
+// var wallsCoors = [
+//   { x: 0.3, y: 0.3, w: 0.02, h: .5 },
+//   { x: 0.25, y: 0.25, w: 0.52, h: 0.02 },
+//   { x: 0.25, y: 0.75, w: 0.52, h: 0.02 },
+//   { x: 0.75, y: 0.25, w: 0.02, h: .50 }
+// ];
 var wallsCoors = [
-  { x: 0.25, y: 0.25, w: 0.02, h: 0.52 },
-  { x: 0.25, y: 0.25, w: 0.52, h: 0.02 },
-  { x: 0.75, y: 0.25, w: 0.02, h: 0.52 },
-  { x: 0.25, y: 0.75, w: 0.52, h: 0.02 }];
+  { x: 0.1, y: 0.1, w: 0.8, h: 0.03 },
+  { x: 0.2, y: 0.2, w: 0.6, h: 0.03 },
+  { x: 0.3, y: 0.3, w: 0.4, h: 0.03 },
+  { x: 0.4, y: 0.4, w: 0.2, h: 0.03 },
+  { x: 0.1, y: 0.8, w: 0.8, h: 0.03 },
+  { x: 0.2, y: 0.7, w: 0.6, h: 0.03 },
+  { x: 0.3, y: 0.6, w: 0.4, h: 0.03 },
+  { x: 0.4, y: 0.5, w: 0.2, h: 0.03 }
+];
 var wallKill = [
-  { x: 0.1, y: 0.9, w: 0.1, h: 0.02 }];
+  { x: 0.465, y: 0.13, w: 0.07, h: 0.07 },
+  { x: 0.465, y: 0.33, w: 0.07, h: 0.07 },
+  { x: 0.465, y: 0.53, w: 0.07, h: 0.07 },
+  { x: 0.465, y: 0.73, w: 0.07, h: 0.07 }];
 var walls = [];
 var disloc = 0;
 var dislocAll = { x: 0, y: 0 };
@@ -26,11 +43,16 @@ var cvsX, cvsY, cvsW, cvsH;
 var timer;
 var deltaTime = 0;
 var keys = { c: false, w: false, a: false, s: false, d: false };
+var playerPos = { x: 0, y: 0 };
 
 
 window.onload = function () {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
+  cvsW = window.innerWidth * .95;
+  cvsH = window.innerHeight * .95;
+  playerPos.x = playerInitialPos.x * cvsW;
+  playerPos.y = playerInitialPos.y * cvsH;
   setWindow();
   document.addEventListener("mousemove", updateMouse);
   document.addEventListener("mousedown", function (e) { mouse.click = true; clickMouse(e); });
@@ -51,10 +73,14 @@ window.onload = function () {
 };
 
 function setWindow() {
+  playerPos.x = playerPos.x / cvsW;
+  playerPos.y = playerPos.y / cvsH;
   cvsW = window.innerWidth * .95;
   cvsH = window.innerHeight * .95;
   canvas.width = cvsW;
   canvas.height = cvsH;
+  playerPos.x = playerPos.x * cvsW;
+  playerPos.y = playerPos.y * cvsH;
   var canvasRect = canvas.getBoundingClientRect();
   cvsX = canvasRect.left;
   cvsY = canvasRect.top;
@@ -172,7 +198,6 @@ class Wall {
     this.y = Math.round(this.origY * this.canvH + dislocAll.y + this.dislocY);
     this.w = Math.round(this.origW * this.canvW);
     this.h = Math.round(this.origH * this.canvH);
-
   }
 
   draw() {
@@ -186,27 +211,30 @@ class Wall {
     this.y = Math.round(this.origY * this.canvH + dislocAll.y + this.dislocY);
   }
 
+
   collide(obj) {
     this.x = Math.round(this.origX * this.canvW + dislocAll.x + this.dislocX);
     this.y = Math.round(this.origY * this.canvH + dislocAll.y + this.dislocY);
     var res = false;
     if (obj.dead) return res;
     if (((obj.y + obj.r) >= this.y && (obj.y - obj.r) <= (this.y + this.h)) || ((obj.y + obj.r + obj.vy * (deltaTime / 20)) >= this.y && (obj.y - obj.r + obj.vy * (deltaTime / 20)) <= (this.y + this.h))) {
-      if (obj.vx > 0 && ((obj.x + obj.r) <= (this.x)) && (obj.x + obj.r + obj.vx * (deltaTime / 20)) >= this.x) {
+      if (obj.vx > 0 && ((((obj.x + obj.r) <= (this.x + dislocAllValue + disloc)) && (obj.x + obj.r + obj.vx * (deltaTime / 20) + dislocAllValue + disloc) >= this.x))) {
         obj.vx = -Math.abs(obj.vx);
-        obj.x = this.x - obj.r - 1 + obj.vx * (deltaTime / 20);
+        obj.vx *= bounciness;
+        obj.x = this.x - obj.r - 1 + obj.vx * (deltaTime / 20) - dislocAllValue - disloc;
         this.dislocX += disloc;
         if (this.x > (1 + 10 * disloc) && this.x < (cvsW - (1 + 10 * disloc + this.w))) {
-          this.x += obj.vx;
+          // this.x += obj.vx;
           dislocAll.x += dislocAllValue;
         }
         res = true;
-      } else if (obj.vx < 0 && (obj.x - obj.r) >= (this.x + this.w) && (obj.x - obj.r + obj.vx * (deltaTime / 20)) <= (this.x + this.w)) {
+      } else if (obj.vx < 0 && (((obj.x - obj.r) >= (this.x + this.w - dislocAllValue - disloc) && (obj.x - obj.r + obj.vx * (deltaTime / 20) - dislocAllValue - disloc) <= (this.x + this.w)))) {
         obj.vx = Math.abs(obj.vx);
-        obj.x = this.x + this.w + obj.r + 1 + obj.vx * (deltaTime / 20);
+        obj.vx *= bounciness;
+        obj.x = this.x + this.w + obj.r + 1 + obj.vx * (deltaTime / 20) + dislocAllValue + disloc;
         this.dislocX -= disloc;
         if (this.x > (1 + 10 * disloc) && this.x < (cvsW - (1 + 10 * disloc + this.w))) {
-          this.x -= obj.vx;
+          //this.x -= obj.vx;
           dislocAll.x -= dislocAllValue;
         }
         res = true;
@@ -214,21 +242,23 @@ class Wall {
     }
 
     if (((obj.x + obj.r) >= this.x && (obj.x - obj.r) <= (this.x + this.w)) || ((obj.x + obj.r + obj.vx * (deltaTime / 20)) >= this.x && (obj.x - obj.r + obj.vx * (deltaTime / 20)) <= (this.x + this.w))) {
-      if (obj.vy > 0 && ((obj.y + obj.r) <= (this.y)) && (obj.y + obj.r + obj.vy * (deltaTime / 20)) >= this.y) {
+      if (obj.vy > 0 && ((((obj.y + obj.r) <= (this.y + dislocAllValue + disloc)) && (obj.y + obj.r + obj.vy * (deltaTime / 20) + dislocAllValue + disloc) >= this.y))) {
         obj.vy = -Math.abs(obj.vy);
-        obj.y = this.y - obj.r - 1 + obj.vy * (deltaTime / 20);
+        obj.vy *= bounciness;
+        obj.y = this.y - obj.r - 1 + obj.vy * (deltaTime / 20) - dislocAllValue - disloc;
         this.dislocY += disloc;
         if (this.y > (1 + 10 * disloc) && this.y < (cvsH - (1 + 10 * disloc + this.h))) {
-          this.y += obj.vy;
+          //this.y += obj.vy;
           dislocAll.y += dislocAllValue;
         }
         res = true;
-      } else if (obj.vy < 0 && (obj.y - obj.r) >= (this.y + this.h) && (obj.y - obj.r + obj.vy * (deltaTime / 20)) <= (this.y + this.h)) {
+      } else if (obj.vy < 0 && (((obj.y - obj.r) >= (this.y + this.h - dislocAllValue - disloc) && (obj.y - obj.r + obj.vy * (deltaTime / 20) - dislocAllValue - disloc) <= (this.y + this.h)))) {
         obj.vy = Math.abs(obj.vy);
-        obj.y = this.y + this.h + obj.r + 1 + obj.vy * (deltaTime / 20);
+        obj.vy *= bounciness;
+        obj.y = this.y + this.h + obj.r + 1 + obj.vy * (deltaTime / 20) + dislocAllValue + disloc;
         this.dislocY -= disloc;
         if (this.y > (1 + 10 * disloc) && this.y < (cvsH - (1 + 10 * disloc + this.h))) {
-          this.y -= obj.vy;
+          //this.y -= obj.vy;
           dislocAll.y -= dislocAllValue;
         }
         res = true;
@@ -267,7 +297,8 @@ class Projectile {
     if (this.dead) return;
     if (Math.abs(this.vx) < 0.001) this.vx += (0.0001) * Math.sign(this.vx);
     if (Math.abs(this.vy) < 0.001) this.vy += (0.0001) * Math.sign(this.vy);
-
+    this.vy = this.vy + g;
+    if (this.vy >= maxV) this.vy = maxV;
     this.x += this.vx * (deltaTime / 20);
     this.y += this.vy * (deltaTime / 20);
     //Bounce
@@ -280,9 +311,11 @@ class Projectile {
     }
     if (this.y > (this.h - this.r)) {
       this.vy = - Math.abs(this.vy);
+      this.vy *= bounciness;
       this.y += (2 * this.vy);
     } else if (this.y < this.r) {
       this.vy = Math.abs(this.vy);
+      this.vy *= bounciness;
       this.y += (2 * this.vy);
     }
     //Wrap
